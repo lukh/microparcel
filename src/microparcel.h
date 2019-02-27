34c0e29ab5ee527820c0b03b13678e5f6d7bd1c3
Vivien Henry <vivien.henry@outlook.fr>
2019-02-27 14:28:14 +0100

    implementation of the microparcel get/set

--- src/microparcel.h
+++ src/microparcel.h
@@ -2,20 +2,121 @@
 #define MICROPARCEL_H
 
 #include <cstdint>
-
+#include <limits>
 
 namespace microparcel{
+    /**
+     * \brief a Message class that provides API to access specific fields of a data payload
+     * \tparam Size: the Byte Size of the Message
+     */
     template <uint8_t Size>
     class Message{
         public:
+            /**
+             * \brief returns bitfields of Bitsize located at Offset in a uint8_t data chunk
+             * Can return field from 1 to 16 bits
+             * \tparam T the return type
+             * \tparam Offset the offset, in bits (from 0 to 8*Size)
+             * \tparam Bitsize the bitsize of the returned field (defines the mask)
+             * */
             template <typename T, uint8_t Offset, uint8_t Bitsize>
             T get(){
+                //check consistency
+                static_assert(std::numeric_limits<T>::digits <= 16, "Can't get data larger than uint16_t");
+                static_assert(Bitsize <= 16, "Bit size is bigger than 16");
+                static_assert(Bitsize > 0, "Bit size can't be zero");
+                
+                static_assert(std::numeric_limits<T>::digits >= Bitsize, "the return type can't handle Bitsize");
+                static_assert((Offset + Bitsize) <= 8 * Size, "Bitsize+Offset is out of range");
 
+                static_assert((Bitsize <= 8) or ((Bitsize > 8) and (Offset & 0x3) == 0), "Offset must be a multiple of 8 for >8 bit bitfield");
+
+                // for <= 8bits:
+                if(Bitsize <= 8){
+                    // on one byte
+                    if((Offset & 0x7) + Bitsize <= 8){
+                        uint8_t mask = (1<<Bitsize) - 1;
+                        uint8_t byte_idx = Offset >> 3;
+                        uint8_t byte_shift = Offset & 0x7;
+                        return (data[byte_idx] >> byte_shift) & mask;
+                    }
+
+                    else{
+                        uint8_t mask = (1<<Bitsize) - 1;
+                        uint8_t byte_idx = Offset >> 3;
+                        uint8_t byte_shift = Offset & 0x7;
+                        uint8_t mask_lsb = mask & ( (1 << (8 - byte_shift)) - 1);
+                        uint8_t mask_msb = mask >> (8 - byte_shift);
+
+                        uint8_t lsb_part = (data[byte_idx] >> byte_shift) & mask_lsb;
+                        uint8_t msb_part = data[byte_idx+1] & mask_msb;
+
+                        return lsb_part | (msb_part << (8 - byte_shift));
+                    }
+                }
+
+                // for >8bits
+                else{
+                    uint16_t mask = (1<<Bitsize) - 1;
+                    uint16_t byte_idx = Offset >> 3;
+
+                    return (data[byte_idx] & (uint8_t)(mask&0xFF)) | ((data[byte_idx+1] & (uint8_t)(mask>>8)) << 8);
+                }
             };
 
             template <typename T, uint8_t Offset, uint8_t Bitsize>
             void set(T field){
+                //check consistency
+                static_assert(std::numeric_limits<T>::digits <= 16, "Can't get data larger than uint16_t");
+                static_assert(Bitsize <= 16, "Bit size is bigger than 16");
+                static_assert(Bitsize > 0, "Bit size can't be zero");
+                
+                static_assert(std::numeric_limits<T>::digits >= Bitsize, "the return type can't handle Bitsize");
+                static_assert((Offset + Bitsize) <= 8 * Size, "Bitsize+Offset is out of range");
 
+                static_assert((Bitsize <= 8) or ((Bitsize > 8) and (Offset & 0x3) == 0), "Offset must be a multiple of 8 for >8 bit bitfield");
+
+                // for <= 8bits:
+                if(Bitsize <= 8){
+                    // on one byte
+                    if((Offset & 0x7) + Bitsize <= 8){
+                        uint8_t mask = (1<<Bitsize) - 1;
+                        uint8_t byte_idx = Offset >> 3;
+                        uint8_t byte_shift = Offset & 0x7;
+                        data[byte_idx] &= ~(mask << byte_shift);
+                        data[byte_idx] |= (field & mask) << byte_shift;
+                        return;
+                    }
+
+                    else{
+                        uint8_t mask = (1<<Bitsize) - 1;
+                        uint8_t byte_idx = Offset >> 3;
+                        uint8_t lsb_byte_shift = Offset & 0x7;
+                        uint8_t msb_byte_shift = 8 - lsb_byte_shift;
+                        uint8_t mask_lsb = mask & ( (1 << msb_byte_shift) - 1);
+                        uint8_t mask_msb = mask >> msb_byte_shift;
+
+                        // lsb
+                        data[byte_idx] &= ~( mask_lsb << lsb_byte_shift );
+                        data[byte_idx] |= (field & mask_lsb) << lsb_byte_shift;
+                        // msb
+                        data[byte_idx+1] &= ~mask_msb;
+                        data[byte_idx+1] |= (field >> msb_byte_shift) & mask_msb;
+                        return; // lsb_part | (msb_part << (8 - byte_shift));
+                    }
+                }
+
+                // for >8bits
+                else{
+                    uint16_t mask = (1<<Bitsize) - 1;
+                    uint8_t byte_idx = Offset >> 3;
+
+                    data[byte_idx] &= ~(mask & 0xFF);
+                    data[byte_idx] |= (field & 0xFF);
+
+                    data[byte_idx+1] &= ~(mask >> 8);
+                    data[byte_idx+1] |= (field >> 8);
+                }
             };
 
         protected:
